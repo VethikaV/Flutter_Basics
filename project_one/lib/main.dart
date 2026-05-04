70da5789dcf7195da62b5a89b5e88c91dd809c90
Vethika <vethika1681@gmail.com>
2026-05-04 22:25:55 +0530

    created a menu

--- project_one/lib/main.dart
+++ project_one/lib/main.dart
@@ -1,49 +1,251 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            
-            //height and width podanum eda container kula
-            height: 200,
-            width: 200,
-            // text center ah allign pananum
-            
-            //curved border ku we use decorator
-            //decoration use pana moothu box color nu sepearate ah container la poda mudiathu so we define colour inside borderradius
-            decoration: BoxDecoration
-            (borderRadius: BorderRadius.circular(10), color:Colors.blue),
-            
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-              Container(
-              decoration: BoxDecoration
-            (borderRadius: BorderRadius.circular(10), color:Colors.red),
-            padding:EdgeInsets.all(10),
-              child: Text('Hello world'),
-            ),
-            Container(
-              decoration: BoxDecoration
-            (borderRadius: BorderRadius.circular(10), color:Colors.red),
-            padding:EdgeInsets.all(10),
-              child: Text('Hello world'),
-            ),
-            ],)
-          ),
+void main(){
+runApp(MaterialApp(
+    home:Scaffold(
+        appBar: AppBar(
+            title:Text("Restaurant Menu"),
+            backgroundColor: Colors.redAccent,
         ),
-      ),
-    );
-  }
+        body:ListView
+        (children: [
+            Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+        // 2nd 
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+        
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+         Container(
+            decoration: BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+               boxShadow: [
+                BoxShadow(
+                   spreadRadius: 2, blurRadius:5,
+                   offset:Offset(0,5),
+                   color: Colors.black26,
+                ),
+               ],
+            ),
+            
+            width: double.infinity,
+            // space around the card margin
+            margin: EdgeInsets.all(10),
+             // container kula space pananum na ega nama padding use pananum
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                 Text("Delicous Chickem noodles with spics"),
+                 Text("30Rs", style: TextStyle(fontSize:18, fontWeight:FontWeight.bold),),
+            ],)
+        ),
+        
+        ],)
+    ),
+));
 }
